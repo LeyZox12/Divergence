343acd73e7b8ce3e0b6ff6d1d6f36548d1d8845c
Azurii <therealazurii@gmail.com>
2025-04-19 13:38:27 +0200

    Updated main.lua
    Cleaned up code and commented a bit

--- main.lua
+++ main.lua
@@ -1,10 +1,10 @@
 DIVERGENCE = {}
+
+--Hook for the easy_dollar function
 DIVERGENCE.onMoney = ease_dollars
-
-
 ease_dollars = function(mod, instant)
     for _, v in ipairs(G.vouchers.cards) do
-      if v.config.center.key == 'v_divergence_eco' and mod > 0 then
+      if v.config.center.key == 'v_divergence_steady_income' and mod > 0 then
         mod = mod +1
       end
     end
@@ -12,9 +12,10 @@ ease_dollars = function(mod, instant)
     return out
 end
 
-DIVERGENCE.onColdBreak = function()
+--When Ice Card breaks
+DIVERGENCE.onIceBreak = function()
   for _, j in ipairs(G.jokers.cards) do
-    if j.config.center.key == 'j_divergence_cold_joker' then
+    if j.config.center.key == 'j_divergence_frozen_joker' then
       local card_ability = j and j.ability or j.config
       card_ability.extra.currMult = card_ability.extra.currMult + card_ability.extra.multGain
       j:juice_up()
@@ -22,6 +23,7 @@ DIVERGENCE.onColdBreak = function()
   end
 end
 
+-- SETUP SPRITESHEETS --
 SMODS.Atlas{
   key = "modicon",
   path = "Icon.png",
@@ -56,7 +58,9 @@ SMODS.Atlas{
   px = 70,
   py = 94,
 }
+----
 
+--Ice Cards Enhancement
 SMODS.Enhancement
 {
   name = "ice",
@@ -74,27 +78,35 @@ SMODS.Enhancement
   replace_base_card = true,
   always_scores = true,
   pos = {x = 0, y = 0},
+    
   loc_vars = function(self, info_queue, card)
     local card_ability = card and card.ability or self.config
     return {
         vars = { card_ability.extra.mult, card_ability.extra.maxRounds, card_ability.extra.currentRounds}
     }
   end,
+    
   update = function(self, card, dt)
       local card_ability = card and card.ability or self.config
       card.children.center:set_sprite_pos({x = 0, y = card_ability.extra.currentRounds})
   end,
-  calculate = function(self, card, context)
     
+  calculate = function(self, card, context)
+    --Card Scoring
     local card_ability = card and card.ability or self.config
     if context.main_scoring and context.cardarea == G.hand  then
-      return {x_chips = 1.25}
+      --In hand
+      if context.cardarea == G.hand then
+        return {x_chips = 1.25}
+      --In played hand
+      elseif context.cardarea == G.play then
+        return {chips = 50}
+      end
     end
-    if context.main_scoring and context.cardarea == G.play then
-      return{chips = 50}
-    end
+        
     if context.destroy_card then
       juice_card(card)
+      --Change Card extra.currentRounds / Shatter
       G.E_MANAGER:add_event(Event({
         trigger = 'after',
         delay = 0.5,
@@ -102,17 +114,20 @@ SMODS.Enhancement
           card_ability.extra.currentRounds = card_ability.extra.currentRounds + 1
           card.children.center:set_sprite_pos({x = 0, y = card_ability.extra.currentRounds})
           if card_ability.extra.currentRounds == card_ability.extra.maxRounds then
-            DIVERGENCE.onColdBreak()
+            DIVERGENCE.onIceBreak()
             card:shatter()
           end
-            return true end }))
+          return true
+      end }))
       return
     end
   end,
+    
   discovered = true,
   atlas = "enhancements"
 }
 
+--Caver Tarot Card
 SMODS.Consumable
 {
   name = "caver",
@@ -126,22 +141,27 @@ SMODS.Consumable
   },
   loc_vars = function(self, info_queue, card)
   info_queue[#info_queue+1] = G.P_CENTERS[self.config.mod_conv]
+        
   local card_ability = card and card.ability or self.config
   return {
       vars = {card_ability.mod_conv, card_ability.max_highlighted}
   }
   end,
+
+  --Amount of cards it can be used on at once
   can_use = 
   function(self)
     return self.config.max_highlighted >= #G.hand.highlighted and #G.hand.highlighted > 0 
   end,
+    
   discovered = true,
   atlas = "tarots"
 }
 
+--Steady Income Voucher
 SMODS.Voucher{
-  name = "steady_economy",
-  key = "eco",
+  name = "steady_income",
+  key = "steady_income",
   pos = {x = 0, y = 0},
   cost = 10,
   config = {
@@ -156,6 +176,7 @@ SMODS.Voucher{
     card_ability.extra.isRedeemed = true
     card_ability.extra.lastMoney = G.GAME.dollars
   end,
+    
   loc_vars = function(self, info_queue, card)
     local card_ability = card and card.ability or self.config
     return {
@@ -163,21 +184,19 @@ SMODS.Voucher{
     }
   end,
 
-  
-
-
   discovered = true,
   atlas = "vouchers"
 }
 
+--Overkill Voucher
 SMODS.Voucher{
   name = "overkill",
-  key = "eco_up",
+  key = "overkill",
   pos = {x = 0, y = 1},
   config = {
     extra = {
       gain = 1,
-      percent = 5,
+      percentage = 5,
       limitMoney = 50,
       moneyGained = 0
     }
@@ -185,33 +204,37 @@ SMODS.Voucher{
   loc_vars = function(self, info_queue, card)
     local card_ability = card and card.ability or self.config
     return {
-        vars = {card_ability.extra.gain, card_ability.extra.percent, card_ability.extra.limitMoney}
+        vars = {card_ability.extra.gain, card_ability.extra.percentage, card_ability.extra.limitMoney}
     }
   end,
+    
   calculate = function(self, card, context)
     local card_ability = card and card.ability or self.config
     if context.end_of_round and context.cardarea == G.vouchers then
-      local moneyGain = math.floor((G.GAME.chips / G.GAME.blind.chips - 1) * 100 / card_ability.extra.percent * card_ability.extra.gain)
+      local moneyGain = math.floor((G.GAME.chips / G.GAME.blind.chips - 1) * 100 / card_ability.extra.percentage * card_ability.extra.gain)
       if moneyGain > card_ability.extra.limitMoney then
         moneyGain = card_ability.extra.limitMoney
       end
       card_ability.extra.moneyGained = moneyGain
     end
   end,
+    
   calc_dollar_bonus = function(self, card)
     local card_ability = card and card.ability or self.config
     local gain = card_ability.extra.moneyGained
     card_ability.extra.moneyGained = 0
     return gain
   end,
-  requires = {'v_divergence_eco'},
+    
+  requires = {'v_divergence_steady_income'}, --Sets Overkill as the upgrade of Steady Income
   discovered = true,
   atlas = "vouchers"
 }
 
+--Frozen Joker
 SMODS.Joker{
-  name = "cold_joker",
-  key = "cold_joker",
+  name = "frozen_joker",
+  key = "frozen_joker",
   config = {
     extra = {
       currMult = 1.0,
@@ -226,12 +249,14 @@ SMODS.Joker{
       vars = {card_ability.extra.currMult, card_ability.extra.multGain}
     }
   end,
+    
   calculate = function(self, card, context)
     local card_ability = card and card.ability or self.config
     if context.joker_main then
       return {x_chips = card_ability.extra.currMult}
     end
   end,
+    
   discovered = true,
   atlas = "jokers"
 }
